969327389bd79fd2ad4bb5c63196ac2fe554f672
salsabeel10 <salsabeelibrahim327@gmail.com>
2025-02-17 12:56:28 +0400

    profile section completed

--- react-portfolio/src/components/Profile.jsx
+++ react-portfolio/src/components/Profile.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
 
 const Profile = () => {
+  const handleDownload = () => {
+    const link = document.createElement('a')
+    link.href = '/assets/Salsabeel-Ibrahim-Resume.pdf' // Make sure the file is inside the 'public' folder
+    link.download = 'Salsabeel_Ibrahim_CV.pdf' // Change the filename as needed
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
   return (
     <div>
       <section id="profile">
-        <div class="section__pic-container">
+        <div className="section__pic-container">
           <img src="./assets/profile-pic1.png" alt="John Doe profile picture" />
         </div>
-        <div class="section__text">
-          <p class="section__text__p1">Hello, I'm</p>
-          <h1 class="title">Salsabeel Ibrahim</h1>
-          <p class="section__text__p2">Software Engineer</p>
-          <div class="btn-container">
-            <button
-              class="btn btn-color-2"
-              onclick="window.open('./assets/Salsabeel-Ibrahim-cv.pdf')"
-            >
+        <div className="section__text">
+          <p className="section__text__p1">Hello, I'm</p>
+          <h1 className="title">Salsabeel Ibrahim</h1>
+          <p className="section__text__p2">Software Engineer</p>
+          <div className="btn-container">
+            <button className="btn btn-color-2" onClick={handleDownload}>
               Download CV
             </button>
             <button
-              class="btn btn-color-1"
-              onclick="location.href='./#contact'"
+              className="btn btn-color-1"
+              onClick={() => (window.location.href = './#contact')}
             >
               Contact Info
             </button>
@@ -29,14 +34,25 @@ const Profile = () => {
             <img
               src="./assets/linkedin.png"
               alt="My LinkedIn profile"
-              class="icon"
-              onclick="location.href='https://linkedin.com/in/salsabeel-ibrahim'"
+              className="icon"
+              onClick={() =>
+                window.open(
+                  'https://linkedin.com/in/salsabeel-ibrahim',
+                  '_blank',
+                  'noopener,noreferrer'
+                )
+              }
             />
             <img
               src="./assets/github.png"
               alt="My Github profile"
-              class="icon"
-              onclick="location.href='https://github.com/salsabeel10'"
+              className="icon"
+              onClick={() =>
+                window.open(
+                  'https://github.com/salsabeel10',
+                  '_blank'
+                )
+              }
             />
           </div>
         </div>
